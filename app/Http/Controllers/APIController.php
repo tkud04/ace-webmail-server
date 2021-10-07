6c020e8b92195e273279007bd29b120b2e186f46
Tkud <kudayisitobi@gmail.com>
2021-10-07 16:14:21 +0100

    adding ui

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,133 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Helpers\Contracts\HelperContract; 
+use Auth;
+use Session; 
+use Cookie;
+use Validator; 
+use Carbon\Carbon;
+use App\User; 
+//use Codedge\Fpdf\Fpdf\Fpdf;
+use PDF;
+
+class APIController extends Controller {
+
+	protected $helpers; //Helpers implementation
+    
+    public function __construct(HelperContract $h)
+    {
+    	$this->helpers = $h;                      
+    }
+
+	
+	/**
+	 * Show the application home page.
+	 *
+	 * @return Response
+	 */
+	public function getIndex(Request $request)
+    {
+		$ret = ['status' => "ok"];
+		return json_encode($ret);
+		
+    }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/**
+	 * Switch user mode (host/guest).
+	 *
+	 * @return Response
+	 */
+	public function getTestBomb(Request $request)
+    {
+		$user = null;
+		$messages = [];
+		$ret = ['status' => "error", 'message' => "nothing happened"];
+		
+		if(Auth::check())
+		{
+			$user = Auth::user();
+			$messages = $this->helpers->getMessages(['user_id' => $user->id]);
+		}
+		else
+		{
+			$ret['message'] = "auth";
+		}
+		
+		$req = $request->all();
+		
+		$validator = Validator::make($req, [
+                             'type' => 'required',
+                             'method' => 'required',
+                             'url' => 'required'
+         ]);
+         
+         if($validator->fails())
+         {
+             $ret['message'] = "validation";
+         }
+		 else
+		 {
+       $rr = [
+          'data' => [],
+          'headers' => [],
+          'url' => $req['url'],
+          'method' => $req['method']
+         ];
+      
+      $dt = [];
+      
+		   switch($req['type'])
+		   {
+		     case "bvn":
+		       /**
+			   $rr['data'] = [
+		         'bvn' => $req['bvn'],
+		         'account_number' => $req['account_number'],
+		        'bank_code' => $req['bank_code'],
+		         ];
+		       **/  
+			   //localhost:8000/tb?url=https://api.paystack.co/bank/resolve_bvn/:22181211888&method=get&type=bvn
+		         $rr['headers'] = [
+		           'Authorization' => "Bearer ".env("PAYSTACK_SECRET_KEY")
+		           ];
+		     break;
+		   }
+		   
+			$ret = $this->helpers->bomb($rr);
+			 
+		 }
+		 
+		 dd($ret);
+    }
+	
+	
+	
+
+	/**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function getZoho()
+    {
+        $ret = "97916613";
+    	return $ret;
+    }
+	
+	
+
+	
+}
---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/', 'APIController@getIndex');
+
+//Authentication
+Route::get('signup', 'APIController@getSignup');
+Route::post('signup', 'APIController@postSignup');
+Route::get('forgot-password', 'APIController@getForgotPassword');
+Route::post('forgot-password', 'APIController@postForgotPassword');
+Route::get('reset', 'APIController@getPasswordReset');
+Route::post('reset', 'APIController@postPasswordReset');
+Route::get('hello', 'APIController@getHello');
+Route::post('hello', 'APIController@postHello');
+Route::get('bye', 'APIController@getBye');
