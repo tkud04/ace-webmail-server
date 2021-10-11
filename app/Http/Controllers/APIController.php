c19caf9f938630c246254508769247e07bacb71f
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-10-11 13:46:20 +0100

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -127,7 +127,8 @@ class APIController extends Controller {
 		{
 		  if($this->helpers->apiAuth($req))
 		  {
-			  $msgs = $this->helpers->getMessages($req['u']);
+			 $l = isset($req['l']) ? $req['l'] : "all";
+			  $msgs = $this->helpers->getMessages($req['u'],$l);
               $ret = ['status' => "ok",'data' => $msgs];		
 		  }
 		  else
